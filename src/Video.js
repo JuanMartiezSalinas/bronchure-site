f2ccb3e9f0d0ad03da07ca79c9ad01ad59676706
JuanMartinezSalinas <62734979+JuanMartiezSalinas@users.noreply.github.com>
2021-03-26 19:31:42 -0600

    Add files via upload

--- src/Video.js
+++ src/Video.js
@@ -2,18 +2,22 @@
 
 import React from "react";
 import ReactPlayer from "react-player";
+// import Iframe from "react-iframe";
 
 const Video = () => {
   // if (window.screen.width > 600) {
   //   width = 50;
   //   height = 50;
   // }
+
   return (
     <div id="video" className="video">
       <div className="wrapper">
         <ReactPlayer
           url="https://www.youtube.com/watch?v=_N7adWvQDM4"
           className="react-player"
+          width="100%"
+          height="100%"
         />
       </div>
     </div>
